e12f428111d812204370279a1cb94c833bfaa386
LucasPIB <lucasoliveira783@gmail.com>
2023-06-14 17:18:36 -0300

    feat: basic aplication and workflow

---
+++ src/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello test flask home page"
+
+
+@app.route("/vowel_count", methods=["POST"])
+def count_vowels():
+    if request.content_type != "application/json":
+        return jsonify({"error": "Content type must be application/json"}), 400
+
+    data = request.get_json()
+    if "words" not in data or not isinstance(data["words"], list):
+        return jsonify({"error": "Invalid request data"}), 400
+
+    result = {}
+    for word in data["words"]:
+        vowel_count = sum(1 for char in word.lower() if char in "aeiou")
+        result[word] = vowel_count
+
+    return jsonify(result)
+
+
+@app.route("/sort", methods=["POST"])
+def sort_words():
+    if request.content_type != "application/json":
+        return jsonify({"error": "Content type must be application/json"}), 400
+
+    data = request.get_json()
+    if "words" not in data or not isinstance(data["words"], list):
+        return jsonify({"error": "Invalid request data"}), 400
+
+    order = data.get("order", "asc")
+    if order not in ["asc", "desc"]:
+        return jsonify({"error": "Invalid sort order"}), 400
+
+    sorted_words = sorted(data["words"], reverse=(order == "desc"))
+    return jsonify(sorted_words)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
---
+++ src/tests/tests.py
@@ -0,0 +1,56 @@
+import json
+import unittest
+
+from app import app
+from flask import Flask
+
+
+class APITestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_vowel_count(self):
+        data = {"words": ["batman", "robin", "coringa"]}
+        response = self.app.post(
+            "/vowel_count", data=json.dumps(data), content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 200)
+        result = json.loads(response.data)
+        self.assertEqual(result, {"batman": 2, "robin": 2, "coringa": 3})
+
+    def test_vowel_count_invalid_request(self):
+        data = {"invalid_key": ["batman", "robin", "coringa"]}
+        response = self.app.post(
+            "/vowel_count", data=json.dumps(data), content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 400)
+        result = json.loads(response.data)
+        self.assertIn("error", result)
+
+    def test_sort_asc(self):
+        data = {"words": ["batman", "robin", "coringa"], "order": "asc"}
+        response = self.app.post(
+            "/sort", data=json.dumps(data), content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 200)
+        result = json.loads(response.data)
+        self.assertEqual(result, ["batman", "coringa", "robin"])
+
+    def test_sort_desc(self):
+        data = {"words": ["batman", "robin", "coringa"], "order": "desc"}
+        response = self.app.post(
+            "/sort", data=json.dumps(data), content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 200)
+        result = json.loads(response.data)
+        self.assertEqual(result, ["robin", "coringa", "batman"])
+
+    def test_sort_invalid_order(self):
+        data = {"words": ["batman", "robin", "coringa"], "order": "invalid"}
+        response = self.app.post(
+            "/sort", data=json.dumps(data), content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 400)
+        result = json.loads(response.data)
+        self.assertIn("error", result)
